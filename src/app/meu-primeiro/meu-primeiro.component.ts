0b4eaf16ab81d774ebb7a5ba34b30fbb2481bc86
Tamara Costa <tamarasantosc@gmail.com>
2022-01-07 22:04:57 -0300

    criado primeiro componente

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector : 'meu-primeiro-componente',
+    template: `
+    <p>Meu primeiro componente em Angular 2 </p>
+    `
+})
+export class MeuPrimeiroComponente {
+
+}

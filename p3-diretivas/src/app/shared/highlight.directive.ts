ce3429680ed269adb6a040cbb082d99b89a11ada
Tamara Costa <tamarasantosc@gmail.com>
2022-01-19 09:25:21 -0300

    input e property binding

--- p3-diretivas/src/app/shared/highlight.directive.ts
+++ p3-diretivas/src/app/shared/highlight.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[highlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @HostBinding('style.backgroundColor') backgroundColor: string = '';
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.backgroundColor = this.highlightColor
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.backgroundColor = this.defaultColor
+  }
+
+  @Input('highlight') highlightColor: string = "gray";//dar um nome com o mesmo nome do seletor, reduz o código lá no input properties, no html.
+  @Input() defaultColor: string = "purple";
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor// fez que navegador já abrisse na cor padrão verde que coloquei no html
+  }
 }

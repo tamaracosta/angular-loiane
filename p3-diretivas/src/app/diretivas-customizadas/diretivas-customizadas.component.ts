21f868b31a41b0120f3a598c9313f2de86cc0fa2
Tamara Costa <tamarasantosc@gmail.com>
2022-01-19 13:42:10 -0300

    criado uma diretiva ngElse

--- p3-diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ p3-diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas-customizadas.component.css']
 })
 export class DiretivasCustomizadasComponent implements OnInit {
-
+  mostrarCursos: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos
+  }
 }

954e0287b7c714f82b9dc38f6a469e0a99a04eb5
Tamara Costa <tamarasantosc@gmail.com>
2022-01-20 12:04:01 -0300

    LogService

--- p4-servicos/src/app/cursos/cursos.service.ts
+++ p4-servicos/src/app/cursos/cursos.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
+import { LogService } from "../shared/log.service";
 
 @Injectable()
 
 export class CursosServices {
 
+    emitirCursoCriado = new EventEmitter<string>();
+    static criouNovoCurso = new EventEmitter<string>();
+
     private cursos:string[] = ['Angular', 'JS', 'Bootstrap'];
 
+    constructor(private logService: LogService){
+        console.log('CursosService');
+    }
+
     getCursos(){
-        return this.cursos
+        this.logService.consoleLog('Obtendo lista de cursos');
+        return this.cursos;
     }
 
     addCurso(curso: string){
+        this.logService.consoleLog(`Criando um novo curso ${curso}`);
         this.cursos.push(curso);
+        this.emitirCursoCriado.emit(curso);
+        CursosServices.criouNovoCurso.emit(curso);
     }
 }

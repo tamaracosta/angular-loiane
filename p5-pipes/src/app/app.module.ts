0f183cb713aafd75ca49a9e77aec42673c889bc5
Tamara Costa <tamarasantosc@gmail.com>
2022-01-20 14:45:10 -0300

    criado um pipe | camelCase

--- p5-pipes/src/app/app.module.ts
+++ p5-pipes/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipeComponent } from './exemplos-pipe/exemplos-pipe.component';
+import { CamelCasePipe } from './camel-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemplosPipeComponent
+    ExemplosPipeComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule

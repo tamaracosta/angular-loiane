58f355d216dcf1d989d62823804ffe2781c24ca3
Tamara Costa <tamarasantosc@gmail.com>
2022-01-20 13:56:43 -0300

    exemplos de pipes

--- p5-pipes/src/app/app.module.ts
+++ p5-pipes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExemplosPipeComponent } from './exemplos-pipe/exemplos-pipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemplosPipeComponent
   ],
   imports: [
     BrowserModule

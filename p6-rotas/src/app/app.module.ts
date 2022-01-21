d5da5b4ec2893522c5dca3b2772f94fec68b6bb0
Tamara Costa <tamarasantosc@gmail.com>
2022-01-21 16:42:43 -0300

    criada rotas mudando (SPA)

--- p6-rotas/src/app/app.module.ts
+++ p6-rotas/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
 
+import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +14,7 @@ import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +27,11 @@ import { routing } from './app.routing';
     BrowserModule,
     MatSliderModule,
     BrowserAnimationsModule,
-    routing
+    routing,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
     
   ],
   providers: [],

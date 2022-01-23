a8aa93cf515833256de5ae1acf1339e30e2ce94f
Tamara Costa <tamarasantosc@gmail.com>
2022-01-23 17:17:23 -0300

    criando módulo de rotas

--- p6-rotas/src/app/app.module.ts
+++ p6-rotas/src/app/app.module.ts
@@ -12,10 +12,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { routing } from './app.routing';
+//import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
 
 
 
@@ -32,7 +33,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
     BrowserModule,
     MatSliderModule,
     BrowserAnimationsModule,
-    routing,
+    //routing,
+    AppRoutingModule,
     MatIconModule,
     MatToolbarModule,
     MatSidenavModule,

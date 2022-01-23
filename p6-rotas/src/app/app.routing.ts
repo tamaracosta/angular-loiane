82985d2495511d16fc168cab2f82acc15ca20493
Tamara Costa <tamarasantosc@gmail.com>
2022-01-23 16:23:49 -0300

    componente para curso não encontrado

--- p6-rotas/src/app/app.routing.ts
+++ p6-rotas/src/app/app.routing.ts
@@ -1,6 +1,8 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -9,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 const APP_ROUTES: Routes = [
     { path: 'cursos', component : CursosComponent },
     { path: 'curso/:id', component : CursoDetalheComponent },
+    { path: 'cursoNaoEncontrado', component : CursoNaoEncontradoComponent },
     { path: 'login', component : LoginComponent},
     { path: '', component : HomeComponent }
 ]

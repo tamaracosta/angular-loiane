b1c5efd58f473dcfa47034a481c1ff11ef3ec250
Tamara Costa <tamarasantosc@gmail.com>
2022-01-24 15:42:04 -0300

    Tela de Login e como não mostrar o Menu (NavBar)

--- p6-rotas/src/app/app.component.ts
+++ p6-rotas/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './login/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'p6-rotas';
+  mostrarMenu: boolean = false;
+
+  constructor(private authService: AuthService){
+    this.authService.mostrarMenuEmitter.subscribe(
+      mostrar => this.mostrarMenu = mostrar
+    )
+  }
+
+  
 }
--- p6-rotas/src/app/login/auth.service.ts
+++ p6-rotas/src/app/login/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter  } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from './usuario';
 
@@ -6,16 +6,19 @@ import { Usuario } from './usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  private usuarioAutenticado : boolean = false;
+  public usuarioAutenticado : boolean = false;
+  mostrarMenuEmitter: EventEmitter<boolean> = new EventEmitter();
   constructor(private router: Router) { }
 
   fazerLogin(usuario: Usuario){
     if(usuario.nome === "usuario@email.com" && usuario.senha === "123456"){
       this.usuarioAutenticado = true;
+      this.mostrarMenuEmitter.emit(true);
       this.router.navigate(['/'])
 
     } else {
       this.usuarioAutenticado = false;
+      this.mostrarMenuEmitter.emit(false);
     }
   }
 

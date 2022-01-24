7868baeadefe3a8ebbeeff683bfeb9427b0b401a
Tamara Costa <tamarasantosc@gmail.com>
2022-01-24 09:12:41 -0300

    rota filha - desenvolvendo telas

--- p6-rotas/src/app/alunos/alunos.component.ts
+++ p6-rotas/src/app/alunos/alunos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlunosService } from './alunos.service';
 
 @Component({
   selector: 'app-alunos',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alunosService: AlunosService) { }
+
+  public alunos: any[] = [];
 
   ngOnInit(): void {
+    this.alunos = this.alunosService.getAlunos()
   }
 
 }
--- p6-rotas/src/app/alunos/alunos.module.ts
+++ p6-rotas/src/app/alunos/alunos.module.ts
@@ -1,15 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
+
 
 
 @NgModule({
     imports: [
         CommonModule,
+        MatListModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatButtonModule,
         AlunosRoutingModule
     ],
     exports: [],
@@ -18,6 +28,6 @@ import { AlunosRoutingModule } from './alunos.routing.module';
         AlunoFormComponent, 
         AlunoDetalheComponent
     ],
-    providers: [],
+    providers: [AlunosService],
 })
 export class AlunosModule { }

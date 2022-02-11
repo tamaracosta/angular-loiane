baa98a88e68fb9931758736db7fa5c0f0bb70567
Tamara Costa <tamarasantosc@gmail.com>
2022-02-10 21:12:34 -0300

    Populando campos com setValue e patchValue (CEP)

--- p7-forms/src/app/template-form/template-form.component.ts
+++ p7-forms/src/app/template-form/template-form.component.ts
@@ -23,24 +23,65 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  consultaCEP(cep: any) {
+  consultaCEP(cep: any, form: any) {
 
     // Nova variável CEP, somente com dígitos
     cep = cep.replace(/\D/g, '');
 
     //Verifica se campo cep possui valor informado.
-    if (cep != "") {
+    if (cep != null && cep != "") {
       //Expressão regular para validar o CEP.
       var validacep = /^[0-9]{8}$/;
 
       //Valida o formato do CEP.
       if (validacep.test(cep)) {
+        this.resetaDadosForm(form)
         this.http.get(`https://viacep.com.br/ws/${cep}/json`)
           .pipe(map(dados => dados))
-          .subscribe(dados => console.log(dados))
+          .subscribe(dados => this.populaDadosForm(dados, form))
       }
     }
 
+  }
+
+  populaDadosForm(dados: any, formulario: any){
+    alert(dados.cep)
+    // form.setValue( {
+    //     nome: null,
+    //     email: null,
+    //     endereco: {
+    //       cep: dados.cep,
+    //       numero: "",
+    //       rua: dados.logradouro,
+    //       bairro: dados.bairro,
+    //       cidade: dados.localidade ,
+    //       estado: dados.uf
+    //     }
+                   
+    // })
+
+    formulario.form.patchValue({
+     
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      
+    });
+  
+  }
+
+  resetaDadosForm(formulario: any) {
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
   
 }

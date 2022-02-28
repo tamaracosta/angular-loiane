f918069129f0f135aee90e46182b7a61f1a2c983
Tamara Costa <tamarasantosc@gmail.com>
2022-02-27 22:42:43 -0300

    rota unsubscribe

--- p8-request-http/src/app/app.module.ts
+++ p8-request-http/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnsubscribeRxjsModule } from './unsubscribe-rxjs/unsubscribe-rxjs.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UnsubscribeRxjsModule
 
   ],
   providers: [],

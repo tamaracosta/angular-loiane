b24328beb59d8a5caeffe368ce1d9243e2cf4ddc
Tamara Costa <tamarasantosc@gmail.com>
2022-02-28 10:35:16 -0300

    serviço de alerta generico

--- p8-request-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ p8-request-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -4,6 +4,7 @@ import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { AlertModalComponent } from 'src/app/shared/alert-modal/alert-modal.component';
+import { AlertModalService } from 'src/app/shared/alert-modal.service';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -22,7 +23,8 @@ export class CursosListaComponent implements OnInit {
 
   constructor(
     private service: CursosService,
-    private modalService: BsModalService
+    // private modalService: BsModalService
+    private alertModalService: AlertModalService
     ) { }
 
   ngOnInit() {
@@ -45,9 +47,10 @@ export class CursosListaComponent implements OnInit {
   }
 
   handleError(){
-    this.bsModalRef = this.modalService.show(AlertModalComponent);
-    this.bsModalRef.content.type = 'danger';
-    this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
+    this.alertModalService.showAlertDanger('Erro ao carregar cursos. Tente novamente mais tarde.')
+    // this.bsModalRef = this.modalService.show(AlertModalComponent);
+    // this.bsModalRef.content.type = 'danger';
+    // this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
   }
 
 }

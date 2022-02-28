e2ecc0a1848e0c8eabb5488553f6285912b0ebed
Tamara Costa <tamarasantosc@gmail.com>
2022-02-28 20:08:24 -0300

    Http: Editando Cursos + Resolver (Rota)

--- p8-request-http/src/app/cursos/cursos-form/cursos-form.component.ts
+++ p8-request-http/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -23,9 +23,10 @@ export class CursosFormComponent implements OnInit {
     private location: Location,
     private route: ActivatedRoute
   ) {
+    const curso = this.route.snapshot.data['curso'];
     this.form = this.fb.group({
-      id: [null],
-      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]]
+      id: [curso.id],
+      nome: [curso.nome, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]]
     })
 
   }
@@ -37,21 +38,24 @@ export class CursosFormComponent implements OnInit {
     // })
 
 
-    this.route.params
-    .pipe(
-      map((params : any)=> params['id']),
-      switchMap(id => this.service.loadByID(id))
-    )
-    .subscribe(curso => this.updateForm(curso));
+    // this.route.params
+    // .pipe(
+    //   map((params : any)=> params['id']),
+    //   switchMap(id => this.service.loadByID(id))
+    // )
+    // .subscribe(curso => this.updateForm(curso));
+
+
+
 
   }
 
-  updateForm(curso: any) {
-    this.form.patchValue({
-      id: curso.id,
-      nome: curso.nome
-    });
-  }
+  // updateForm(curso: any) {
+  //   this.form.patchValue({
+  //     id: curso.id,
+  //     nome: curso.nome
+  //   });
+  // }
 
   hasError(field: string): any {
     return this.form.get(field)?.errors;

ab402f42e997f2939e6ac31bcba5b5233c766902
Tamara Costa <tamarasantosc@gmail.com>
2022-03-02 20:40:56 -0300

    Upload de Arquivo: Formulário Upload com Bootstrap

---
+++ p8-request-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.scss']
+})
+export class UploadFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onChange(event: any) {
+    const selectedFiles = <FileList>event.srcElement.files;
+
+    const fileNames = [];
+    for (let i = 0; i < selectedFiles.length; i++) {
+      fileNames.push(selectedFiles[i].name);
+
+    }
+
+    (document.getElementById('customFileLabel') as HTMLElement).innerHTML = fileNames.join(', ');
+  }
+
+}

fd8fd1de4612927af5f55dcd1cba1a674e7d5105
Tamara Costa <tamarasantosc@gmail.com>
2022-03-02 11:18:39 -0300

    atualizando cursos

--- p8-request-http/src/app/cursos/cursos-form/cursos-form.component.ts
+++ p8-request-http/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -63,18 +63,46 @@ export class CursosFormComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
+
     if (this.form.valid) {
-      console.log('submit')
+      console.log('submit');
+
+      let msgSuccess = 'Curso criado com sucesso!';
+      let msgError = 'Erro ao criar curso, tente novamente!';
+      if (this.form.value.id) {
+        msgSuccess = 'Curso atualizado com sucesso!';
+        msgError = 'Erro ao atualizar curso, tente novamente!';
+      }
+
+      this.service.save(this.form.value).subscribe(
+        success => {
+          this.modal.showAlertSuccess(msgSuccess);
+            this.location.back();
+        },
+        error => this.modal.showAlertDanger(msgError)
+      );
     }
 
-    this.service.create(this.form.value).subscribe(
-      sucess => {
-        this.modal.showAlertSuccess('Curso criado com sucesso'),
-          this.location.back()
-      },
-      error => this.modal.showAlertDanger('Erro ao criar o curso. Tente novamente.'),
-      () => console.log('request completo')
-    )
+    // if (this.form.value.id) {
+    //   this.service.update(this.form.value).subscribe(
+    //     sucess => {
+    //       this.modal.showAlertSuccess('Curso atualizado com sucesso'),
+    //         this.location.back()
+    //     },
+    //     error => this.modal.showAlertDanger('Erro ao atualizar o curso. Tente novamente.'),
+    //     () => console.log('update completo')
+    //   )
+    // } else {
+    //   this.service.create(this.form.value).subscribe(
+    //     sucess => {
+    //       this.modal.showAlertSuccess('Curso criado com sucesso'),
+    //         this.location.back()
+    //     },
+    //     error => this.modal.showAlertDanger('Erro ao criar o curso. Tente novamente.'),
+    //     () => console.log('request completo')
+    //   )
+    // }
+
 
   }
 

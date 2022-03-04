fdb9d155e0b335ab9326eedf5f86edf2191fd980
Tamara Costa <tamarasantosc@gmail.com>
2022-03-04 14:47:11 -0300

    deploy

--- p8-request-http/src/polyfills.ts
+++ p8-request-http/src/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 
-/**
+/*
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  * because those flags need to be set before `zone.js` being loaded, and webpack
@@ -31,16 +31,19 @@
  *
  * The following flags will work for all browsers.
  *
- * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
- *
+ */
+ (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+ (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+ (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+ /*
  *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
  *  with the following flag, it will bypass `zone.js` patch for IE/Edge
  *
- *  (window as any).__Zone_enable_cross_context_check = true;
- *
  */
+ (window as any).__Zone_enable_cross_context_check = true;
+
+ /*
+
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
